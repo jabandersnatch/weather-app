a3afffb555c250b1b11a7001180870225d2ed537
jabandersnatch <ja.mendez@uniandes.edu.co>
2023-08-13 13:58:29 -0500

    refactor(title name): refactor the title of the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Home = () => (
   <main className='flex min-h-screen flex-col items-center justify-between p-24 dark:bg-gray-800 dark:text-white bg-white text-black'>
-    <h1 className='text-6xl font-bold text-center sm:text-7xl dark:text-white hover:text-cyan-700'>Next.js + Tailwind CSS</h1>
+    <h1 className='text-6xl font-bold text-center sm:text-7xl dark:text-white hover:text-cyan-700'>Solvedez Weather App</h1>
   </main>
 );
 
